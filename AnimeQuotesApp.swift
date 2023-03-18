7ed12993411a36811e7211495db92325640887a6
YoucefMac <youcefbou1998@gmail.com>
2023-03-18 17:47:31 +0100

    initial commit

---
+++ AnimeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuotesApp.swift
+//  AnimeQuotes
+//
+//  Created by apple on 18/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
